2508709d1f817ae49c1dd01ce4743b09258bac26
Ahmad khaled Elshafee <60294043+elshafee@users.noreply.github.com>
2022-09-05 23:48:42 +0200

    reiit

--- app/src/main/java/com/apps/fullandroidcourseclassd/todolistapp/model/Todo.kt
+++
@@ -1,3 +0,0 @@
-package com.apps.fullandroidcourseclassd.todolistapp.model
-
-data class Todo(val title:String , var isChecked:Boolean) {}
--- app/src/main/java/com/apps/fullandroidcourseclassd/ui/fragments/InboxFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.apps.fullandroidcourseclassd.ui.fragments
-
-import androidx.fragment.app.Fragment
-import com.apps.fullandroidcourseclassd.R
-
-class InboxFragment :Fragment(R.layout.fragment_inbox){
-}
